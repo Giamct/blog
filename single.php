3e4e31974c5045a8e6192e61dec61d77fa1be2e1
GeorgiChaina <georgiactx7@gmail.com>
2022-05-11 22:00:33 -0500

    custom fields

--- single.php
+++ single.php
@@ -2,15 +2,21 @@
 
 <section>
 	<center>
-		<?php the_post_thumbnail("large");?>
 
 		<h3> <?php the_title (); ?></h3>
 
+		<h6> <?php the_field ('resumen'); ?> </h6>
+
+		<?php the_post_thumbnail("large");?>
+
 		<?php the_content (); ?>
 
+		<h6> Fuente : <?php the_field ('fuente'); ?> </h6>
+
+
+
+
         <ul>
-
-
 
 
 
